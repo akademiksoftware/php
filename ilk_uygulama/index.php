3584a8b30aabfd80d678472704c82079be396256
Gultekin Ozdemir <155580330+akademiksoftware@users.noreply.github.com>
2024-10-10 00:34:17 +0300

    php ilk uygulama dosyası eklendi

---
+++ ilk_uygulama/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>İLK UYGULAMA</title>
+</head>
+<body>
+
+<?php $title="PHP Dersleri"; ?>
+<h1> <?php echo $title;  ?> </h1>
+<br>
+<?php $title1="İlk Ders Uygulaması"; ?>
+<h2> <?php echo $title1;  ?> </h2>
+
+
+</body>
+</html>
